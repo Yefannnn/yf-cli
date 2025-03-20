989700c8c7c304eba11df77d583217fa3fae7cbf
Yefan <yefan97114@qq.com>
2025-03-20 11:13:26 +0800

    feat: 更改模板label

--- src/command/create.ts
+++ src/command/create.ts
@@ -20,47 +20,11 @@ export interface TemplateInfo_ {
     },
 }
 
-
-// 这里保存了我写好了咱们的之前开发的模板
-// export const templates: Map<string, TemplateInfo> = new Map(Object.entries({
-//     'admin-vue3-ts-template': {
-//         name: "admin-pro",
-//         downloadUrl: 'git@gitee.com:yefan008/admin-vue3-ts.git',
-//         description: 'Vue3技术栈开发模板',
-//         branch: 'master'
-//     },
-//     'React18-Vite5-Ts-template': {
-//         name: "react18+vite5+ts",
-//         downloadUrl: 'git@gitee.com:yefan008/admin-react-ts.git',
-//         description: 'React18技术栈开发模板',
-//         branch: 'master'
-//     },
-//     'admin-vue3-box-template': {
-//         name: "react18+vite5+ts",
-//         downloadUrl: 'git@gitee.com:yefan008/admin-vue3-box.git',
-//         description: '基于Vue3的后台管理系统模板(开源项目)',
-//         branch: 'master'
-//     },
-//     'fantastic-admin-vue3-template': {
-//         name: "fantastic-admin",
-//         downloadUrl: 'git@gitee.com:fantastic-admin/basic.git',
-//         description: '基于Vue3的后台管理系统模板(开源项目)',
-//         branch: 'main'
-//     },
-//     'fantastic-admin-vue2-template': {
-//         name: "fantastic-admin",
-//         downloadUrl: 'git@gitee.com:fantastic-admin/basic.git',
-//         description: '基于Vue3的后台管理系统模板(开源项目)',
-//         branch: 'vue2'
-//     },
-
-// }))
-
 export const templates_: TemplateInfo_ = {
     'Vue': {
         'Vue2': [
             {
-                label: 'fantastic-admin-vue2-template',
+                label: 'fantastic-admin-vue2-basic-template',
                 name: "fantastic-admin",
                 downloadUrl: 'git@gitee.com:fantastic-admin/basic.git',
                 description: '基于Vue3的后台管理系统模板(开源项目)',
@@ -83,7 +47,7 @@ export const templates_: TemplateInfo_ = {
                 branch: 'master'
             },
             {
-                label: 'fantastic-admin-vue3-template',
+                label: 'fantastic-admin-vue3-basic-template',
                 name: "fantastic-admin",
                 downloadUrl: 'git@gitee.com:fantastic-admin/basic.git',
                 description: '基于Vue3的后台管理系统模板(开源项目)',
@@ -107,14 +71,6 @@ export const templates_: TemplateInfo_ = {
 
 
 export const create = async (prjName?: string) => {
-    // const templateList = Array.from(templates).map((item: [string, TemplateInfo]) => {
-    //     const [name, info] = item
-    //     return {
-    //         name,
-    //         value: name,
-    //         description: info.description
-    //     }
-    // })
     // 文件名称未传入需要输入
     if (!prjName) prjName = await input({ message: '请输入项目名称' });
     // 如果文件已存在需要让用户判断是否覆盖原文件
